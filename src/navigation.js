326df50073fe77b3b3e9f02d7079bf874212ff90
Patricio Avila <pavila@vialterna.com>
2024-06-14 15:25:16 -0600

    Trends page

--- src/navigation.js
+++ src/navigation.js
@@ -7,7 +7,8 @@ trendingBtn.addEventListener('click', () => {
 })
 
 arrowBtn.addEventListener('click', () => {
-    location.hash = '#home'
+    history.back()
+    // location.hash = '#home'
 })
 
 window.addEventListener('DOMContentLoaded', navigator, false)
@@ -126,4 +127,8 @@ function trendsPage() {
     categoriesPreviewSection.classList.add('inactive')
     genericSection.classList.remove('inactive')
     movieDetailSection.classList.add('inactive')
+
+    headerCategoryTitle.innerHTML = 'Trends'
+
+    getTrendingMovies()
 }
